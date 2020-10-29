65fbc6ba6ddd0b3a04cfdb3ab9017883b02cfed3
DariasKehl <neocount@gmail.com>
2020-10-29 19:03:40 -0400

    commit tree 8d19a16ea5fb41f02d8e1e11e3b725fa8c9a1835

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names) #returns an array of greetings
+    retval = []  #This is a shitty variable name
+    names.each {|n| retval << badge_maker(n) }
+    return retval
+end
+
+def assign_rooms(array)
+    retval = []
+    array.each_with_index {|name, index|
+        retval << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    }
+    return retval
+end
+
+def printer(arrayOfNames)
+    weDontNeedNoStinkingBadges = batch_badge_creator(arrayOfNames)
+    communal_living = assign_rooms(arrayOfNames)
+    counter = 1
+    while counter < arrayOfNames.length do
+        puts weDontNeedNoStinkingBadges[counter]
+        puts communal_living[counter]
+        counter += 1
+    end
+end
+
+
+
+
